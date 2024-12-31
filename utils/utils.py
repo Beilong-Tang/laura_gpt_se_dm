0fc8a285eed1eabc6be3435ff98fb6e8cc87a801
Beilong <bt132@duke.edu>
2024-12-31 14:35:48 +0800

    update

--- utils/utils.py
+++ utils/utils.py
@@ -101,3 +101,19 @@ class AttrDict(Namespace):
             return super().__getattribute__(name)
         except AttributeError:
             return None
+
+def get_source_list(file_path: str, ret_name=False):
+    files = []
+    names = []
+    with open(file_path, "r") as f:
+        for line in f.readlines():
+            l = line.replace("\n", "").split(" ")
+            name = l[0]
+            path = l[-1]
+            files.append(path)
+            names.append(name)
+    if ret_name:
+        return names, files
+    return files
+
+
