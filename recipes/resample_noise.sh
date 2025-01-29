6e39ea75055deb4769b08b9d0e67286045f978f2
Beilong <bt132@duke.edu>
2025-01-29 10:32:14 +0800

    update

---
+++ recipes/resample_noise.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Input file
+input_scp="/DKUdata/tangbl/data/urgent2025_challenge/data/noise_train.scp"
+base_path="/DKUdata/tangbl/data/urgent2025_challenge"
+
+# Output directory for resampled audio
+target_sr=16000
+output_dir="/DKUdata/tangbl/data/urgent2025_challenge/resampled/noise_$target_sr"
+mkdir -p "$output_dir"
+
+# Function to resample audio
+resample_audio() {
+    uid="$1"
+    fr="$2"
+    input_path="$3"
+    output_path="$output_dir/$input_path"
+
+    mkdir -p "$(dirname "$output_path")"
+    
+    # echo $uid
+    # echo $fr
+    # echo $input_path
+
+    # Resample to 16kHz (adjust -ar value as needed)
+    ffmpeg -i "$base_path/$input_path" -map_channel 0.0.0 -ar $target_sr "$output_path" -y
+    # ffmpeg -i "$full_input_path" -map_channel 0.0.0 -ar 16000 "$output_path" -y
+    # echo $output_path
+    # echo "$base_path/$input_path"
+}
+
+# Export the function to make it available to xargs
+export -f resample_audio
+export output_dir
+export base_path
+export target_sr
+
+# Use xargs to run resample_audio in parallel
+cat "$input_scp" | xargs -n 3 -P 12 bash -c 'resample_audio "$@"' _
+
+echo "Done"
+
--- recipes/resample_scp_python.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python recipes/resample_scp.py --scp "/DKUdata/tangbl/data/urgent2025_challenge/data/speech_train_track1/gen/spk2fs2scp.scp" \
-    --base_path "/DKUdata/tangbl/data/urgent2025_challenge" \
-    --output_dir "/DKUdata/tangbl/data/urgent2025_challenge/resampled/speech" \
-    --sample_rate 16000 \
-    --num_proc 16
