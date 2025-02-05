945439aa57c15a2679d344108e745b74487fd5de
Beilong <bt132@duke.edu>
2025-02-05 13:43:55 +0800

    update

--- _funcodec.py
+++ _funcodec.py
@@ -4,7 +4,6 @@ from funcodec.train.distributed_utils import DistributedOption
 from funcodec.tasks.text2audio_generation import Text2AudioGenTask
 from funcodec.iterators.sequence_iter_factory import SequenceIterFactory
 
-from _funcodec_data.build_sequence_iter import build_sequence_iter_factory
 
 
 def init_sequence_iter_factory(args, rank, mode) -> SequenceIterFactory:
@@ -29,4 +28,5 @@ def init_dm_sequence_iter_factory(args, rank, mode) -> SequenceIterFactory:
 
     distributed_option.dist_world_size = torch.distributed.get_world_size()
     iter_option = Text2AudioGenTask.build_iter_options(args, distributed_option, mode)
+    from _funcodec_data.build_sequence_iter import build_sequence_iter_factory
     return build_sequence_iter_factory(args, iter_option, mode)
