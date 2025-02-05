b4c00b573230d5a50dc9eac2af0767b162a80162
Beilong <bt132@duke.edu>
2025-02-05 10:41:23 +0800

    update

--- utils/hinter.py
+++ utils/hinter.py
@@ -15,9 +15,9 @@ def hint_once(content, uid, rank=None):
     if (rank is None) or (_cur_rank is None) or _cur_rank == rank:
         if uid not in HINTED:
             if _cur_rank is not None:
-                logging.info(f"[HINT_ONCE] Rank {_cur_rank}: {content}")
+                print(f"[HINT_ONCE] Rank {_cur_rank}: {content}")
             else:
-                logging.info(f"[HINT_ONCE] {content}")
+                print(f"[HINT_ONCE] {content}")
             HINTED.add(uid)
 
 def check_hint(uid) -> bool:
