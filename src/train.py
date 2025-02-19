aeb5cc7cb0756ea6237bd79cb4a4346cb4f7de0f
Beilong <bt132@duke.edu>
2025-02-19 08:58:23 +0800

    update

--- src/train.py
+++ src/train.py
@@ -75,7 +75,7 @@ def main(rank, args):
     model.cuda()
     l.info(f"model {model} is intialized")
     l.info(f"model parameters: {sum(p.numel() for p in model.parameters())}")
-    l.info(f"{sum(p.numel() for p in model.codec_lm.parameters())}")
+    l.info(f"Decoder LM parameters: {sum(p.numel() for p in model.codec_lm.parameters())}")
     for p in args.init_param:
         l.info(f"Loading pretrained params from {p}")
         load_pretrained_model(
