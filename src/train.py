dbff0b5445fcfc4e9f54aee957b68e4e1ba1c241
Beilong <bt132@duke.edu>
2025-02-19 08:20:26 +0800

    update

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import sys 
-sys.path.append(os.path.dirname(os.path(__file__)))
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 import random
 import yaml
